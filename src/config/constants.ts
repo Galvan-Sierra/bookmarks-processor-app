ee3e29fce70977e8e01df8ca013f4cb4d9bc43da
David Galvan <galvan.dsierra@gmail.com>
2025-08-10 14:31:56 -0500

    feat: :sparkles: implement HTML parser and file handler for bookmark management

---
+++ src/config/constants.ts
@@ -0,0 +1,13 @@
+export const HTML_TEMPLATE = (
+  title: string
+) => `<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<!-- This is an automatically generated file.
+     It will be read and overwritten.
+     DO NOT EDIT! -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>${title} - Bookmarks</TITLE>
+<H1>${title} - Bookmarks</H1>
+<DL><p>
+    <DT><H3 PERSONAL_TOOLBAR_FOLDER="true">Marcadores</H3>
+    <DL><p>
+`;
--- src/utils/file-handler.ts
+++ src/utils/file-handler.ts
@@ -1,1 +1,20 @@
-export class FileHandler {}
+export class FileHandler {
+  async read(path: string): Promise<string> {
+    try {
+      const content = await Bun.file(path).text();
+      return content;
+    } catch (error) {
+      console.error(`⚠ Error al leer el archivo ${path}`);
+      throw new Error(`No se pudo cargar el archivo: ${error}`);
+    }
+  }
+
+  async write(path: string, data: string): Promise<void> {
+    try {
+      await Bun.write(path, data);
+    } catch (error) {
+      console.error(`⚠ Error al guardar el archivo  '${path}'`);
+      throw new Error(`No se pudo guardar el archivo de marcadores: ${error}`);
+    }
+  }
+}
