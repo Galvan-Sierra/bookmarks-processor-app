66828f578eee924d425bc32728f9f9a9b3f83dc2
david g <galvan.dsierra@gmail.com>
2025-09-07 16:33:18 -0500

    feat: :sparkles: add BookmarkManager class and implement read method

--- src/core/bookmark-manager.ts
+++ src/core/bookmark-manager.ts
@@ -1,3 +1,27 @@
+import { HtmlParser } from '@parsers/html-parser';
+import { BookmarkService } from '@services/bookmark.service';
+import type { Bookmark } from '@type/bookmark';
+import { FileHandler } from '@utils/file-handler';
+
 export class BookmarkManager {
-  // TODO: Implement bookmark management logic
+  private bookmarkService = new BookmarkService();
+  private fileHandler = new FileHandler();
+  private parser = new HtmlParser();
+
+  private isLoaded: boolean = false;
+
+  constructor(private path: string) {}
+
+  async read(): Promise<void> {
+    try {
+      const content = await this.fileHandler.readFile(this.path);
+      const bookmarks = this.parser.parse(content);
+      this.bookmarkService.add(bookmarks);
+
+      this.isLoaded = true;
+      console.log(`📚 Se han cargado ${bookmarks.length} marcadores`);
+    } catch (error: any) {
+      console.error(`⚠ ${error.message}`);
+    }
+  }
 }
--- src/services/bookmark.service.ts
+++ src/services/bookmark.service.ts
@@ -1,3 +1,25 @@
+import type { Bookmark } from '@type/bookmark';
+
 export class BookmarkService {
-  // TODO: Implement bookmark service logic
+  private bookmarks: Bookmark[] = [];
+  private hrefSet: Set<string> = new Set();
+
+  add(bookmarks: Bookmark[]): void {
+    if (bookmarks.length === 0) return;
+
+    bookmarks.forEach((bookmark) => {
+      if (!this.exists(bookmark.href)) {
+        this.hrefSet.add(bookmark.href);
+        this.bookmarks.push(bookmark);
+      }
+    });
+  }
+
+  get(): Bookmark[] {
+    return [...this.bookmarks];
+  }
+
+  private exists(href: string): boolean {
+    return this.hrefSet.has(href);
+  }
 }
