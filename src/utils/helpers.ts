575a16aef1e27467c1d05fd649d23f30a3a934d1
david g <galvan.dsierra@gmail.com>
2025-09-07 18:06:16 -0500

    feat: add  function to read directories

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,8 @@
-export function someHelper() {
-  // TODO: Implement helper function
+import { readdir } from 'node:fs/promises';
+
+export async function readDirectory(path: string): Promise<string[]> {
+  const normalizePath = path.replaceAll('/', '\\');
+
+  const files = await readdir(normalizePath, { recursive: true });
+  return files.map((file) => normalizePath + file);
 }
