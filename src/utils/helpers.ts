6d896d2b7f7f62f4118845e1a215ca9f75c4956d
david g <galvan.dsierra@gmail.com>
2025-09-08 09:56:19 -0500

    feat: :sparkles: add getUniqueBookmarkValuesByProperty helper funtion

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,4 @@
+import type { Bookmark } from '@type/bookmark';
 import { readdir } from 'node:fs/promises';
 
 export async function readDirectory(path: string): Promise<string[]> {
@@ -5,4 +6,21 @@ export async function readDirectory(path: string): Promise<string[]> {
 
   const files = await readdir(normalizePath, { recursive: true });
   return files.map((file) => normalizePath + file);
+}
+
+export function getUniqueBookmarkValuesByProperty(
+  bookmarks: Bookmark[],
+  property: 'title' | 'href'
+): Bookmark[] {
+  const uniqueValues = new Map<string, Bookmark>();
+
+  if (!bookmarks?.length) return [];
+
+  bookmarks.forEach((bookmark) => {
+    if (!uniqueValues.has(bookmark[property])) {
+      uniqueValues.set(bookmark[property], bookmark);
+    }
+  });
+
+  return Array.from(uniqueValues.values());
 }
